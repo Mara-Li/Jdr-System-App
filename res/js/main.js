a1445ea0c9af93989d338d51bc051d4494104713
Lisandra <mara-li@outlook.fr>
2020-07-11 21:20:27 +0200

    AAAAAH fixed it

--- res/js/main.js
+++ res/js/main.js
@@ -37,8 +37,45 @@ window.onload = function() {
       this.valueAsNumber = elem_inputs.pv_max.valueAsNumber;
     }
   }
+
   elem_inputs.pv_max.onchange();
   elem_inputs.pv_reste.onchange();
+
+	elem_inputs.des_atq.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.des_atq.onchange();
+
+	elem_inputs.des_def.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.des_def.onchange();
+
+	elem_inputs.endurance.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.endurance.onchange();
+
+	elem_inputs.agi.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.agi.onchange();
+
+	elem_inputs.bouclier.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.bouclier.onchange();
+
+	elem_inputs.bonus.onchange = function ()
+	{
+		capage(this, 0, 10);
+	}
+	elem_inputs.bonus.onchange();
 
   if(localStorage.logs){
     logs = JSON.parse(localStorage.getItem("logs"));
@@ -119,8 +156,7 @@ function log_ecriture(){
   logs[logs.length] = log;
   localStorage.setItem("logs", JSON.stringify(logs));
   elem_inputs.log_box.innerHTML = log.toHtml()+"<hr>"+elem_inputs.log_box.innerHTML;
-} //A faire plus tard quand le cadre aura été fait, partie 2 du programme
-//s'efface quand on actualise !
+}
 
 function clearLog(){
   elem_inputs.log_box.innerHTML = '';
@@ -177,32 +213,5 @@ function capage(elem, min=NaN, max=NaN){
   }
   if(isNaN(elem.valueAsNumber) || test_none(elem.value)){
     elem.value = min;
-  }
-}
-
-function capage_change(){
-  var int_pv_max=parseInt(elem_inputs.pv_max.value);
-  var int_atq=parseInt(elem_inputs.des_atq.value);
-  var int_shield=parseInt(elem_inputs.bouclier.value);
-  var int_dice_endu=parseInt(elem_inputs.endurance.value);
-	var int_dice_agi=parseInt(elem_inputs.agi.value);
-  var int_pv_restant=parseInt(elem_inputs.pv_reste.valu);
-  var int_bonus=parseInt(elem_inputs.bonus.value);
-  var int_defense=parseInt(elem_inputs.des_def.value);
-
-  if ((int_atq>10) || (int_atq<0)){
-    elem_inputs.des_atq.onchange=function(){capage(this, 0, 10)}
-  }
-  if ((int_defense >10)||(int_defense<0)){
-    elem_inputs.des_def.onchange=function(){capage(this, 0, 10)}
-  }
-  if ((int_dice_endu >10)||(int_dice_endu<0)) {
-    elem_inputs.endurance.onchange=function(){capage(this, 0, 10)}
-  }
-  if ((int_bonus >100)||(int_bonus<0)){
-    elem_inputs.bonus.onchange=function(){capage(this, 0, 100)}
-  }
-  if ((int_shield >100)||(int_shield<0)){
-    elem_inputs.bouclier.onchange=function(){capage(this, 0, 100)}
   }
 }
