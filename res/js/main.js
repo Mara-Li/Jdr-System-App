c38fd8531f51d08ee4d83222b6ad976940395428
Lisandra <mara-li@outlook.fr>
2020-07-15 16:56:21 +0200

    Toggle dark/light theme

--- res/js/main.js
+++ res/js/main.js
@@ -9,6 +9,9 @@ window.onload = function() {
 	elem_inputs.refresh();
 	capa_toggle(elem_inputs.atq_type);
 	def_toggle();
+	var cookie = readCookie("style");
+  var title = cookie ? cookie : getPreferredStyleSheet();
+  setActiveStyleSheet(title);
 
 	elem_inputs.pv_max["old_value"] = elem_inputs.pv_max.valueAsNumber;
 
@@ -201,3 +204,76 @@ function capage(elem, min = NaN, max = NaN) {
 		elem.value = min;
 	}
 }
+
+function createCookie(name,value,days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime()+(days*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+  }
+  else expires = "";
+  document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function setActiveStyleSheet(title) {
+  var i, a, main;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+      a.disabled = true;
+      if(a.getAttribute("title") == title) a.disabled = false;
+    }
+  }
+}
+
+function getActiveStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function getPreferredStyleSheet() {
+  var i, a;
+  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+    if(a.getAttribute("rel").indexOf("style") != -1
+       && a.getAttribute("rel").indexOf("alt") == -1
+       && a.getAttribute("title")
+       ) return a.getAttribute("title");
+  }
+  return null;
+}
+
+function toggletheme(){
+	var theme=getActiveStyleSheet();
+	if (theme=="dark"){
+		document.getElementById('theme').click=setActiveStyleSheet ('light');
+		return false;
+	}
+	else if (theme=="light") {
+		document.getElementById('theme').click=setActiveStyleSheet ('dark');
+		return false;
+	}
+}
+
+
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+window.onunload = function(e) {
+  var title = getActiveStyleSheet();
+  createCookie("style", title, 365);
+}
+
+
+var cookie = readCookie("style");
+var title = cookie ? cookie : getPreferredStyleSheet();
+setActiveStyleSheet(title);
